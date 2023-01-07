879216b7be7d220a06f169ebe6088b5562b962cd
BCarta7 <cannolidude@msn.com>
2023-01-06 16:29:02 -0800

    end of day commit

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,11 +1,20 @@
 import React from "react"
 
-const Home = () => {
-  return (
-    <>
-      <h3>Home</h3>
-    </>
-  )
-}
+const Home = ({logged_in}) => {
+  console.log("logged_in:", logged_in)
+    if (logged_in === true) {
+      return (
+        <>
+        <h1>Signed in!</h1>
+        </>
+      )
+    } else {
+      return (
+        <>
+        <h1>Signed out  :c</h1>
+        </>
+      )
+    }
+  }
 
 export default Home
