d722a255c5bb0c83279f026f1d1c886c11c8b7ce
BCarta7 <cannolidude@msn.com>
2023-01-09 10:51:12 -0800

    pre-merge refactor

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -2,10 +2,7 @@ import React from "react"
 import { NavLink } from 'reactstrap'
 
 const Home = (props) => {
-  const logged_in = props.logged_in
-  const in_route = props.sign_in_route
-  const up_route = props.new_user_route
-  console.log(logged_in)
+  const { logged_in, sign_in_route, new_user_route} = props
     if (logged_in === true) {
       return (
         <>
@@ -24,10 +21,10 @@ const Home = (props) => {
         <>
         <h1>WELCOME! Please sign in</h1>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus enim harum quidem quia facilis, repellendus suscipit ducimus eveniet assumenda, blanditiis libero itaque delectus rerum culpa modi! Maiores distinctio veniam magni.</p>
-        <NavLink href= {in_route}>
+        <NavLink href= {sign_in_route}>
           <button>Sign in</button>
         </NavLink>
-        <NavLink href={up_route}>
+        <NavLink href={new_user_route}>
           <button>Sign up</button>
         </NavLink>
         </>
