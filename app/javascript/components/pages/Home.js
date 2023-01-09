1a18eceba226b4f3f0a07e7b49d61ce3a5a039f5
BCarta7 <cannolidude@msn.com>
2023-01-09 10:29:10 -0800

    complete

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,17 +1,35 @@
 import React from "react"
+import { NavLink } from 'reactstrap'
 
-const Home = ({logged_in}) => {
-  console.log("logged_in:", logged_in)
+const Home = (props) => {
+  const logged_in = props.logged_in
+  const in_route = props.sign_in_route
+  const up_route = props.new_user_route
+  console.log(logged_in)
     if (logged_in === true) {
       return (
         <>
-        <h1>Signed in!</h1>
+        <h1>WELCOME!</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus enim harum quidem quia facilis, repellendus suscipit ducimus eveniet assumenda, blanditiis libero itaque delectus rerum culpa modi! Maiores distinctio veniam magni.</p>
+        <NavLink href='/apartmentindex'>
+          <button>My Listings</button>
+        </NavLink>
+        <NavLink href='/apartmentindex'>
+          <button>View all listings</button>
+        </NavLink>
         </>
       )
     } else {
       return (
         <>
-        <h1>Signed out  :c</h1>
+        <h1>WELCOME! Please sign in</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus enim harum quidem quia facilis, repellendus suscipit ducimus eveniet assumenda, blanditiis libero itaque delectus rerum culpa modi! Maiores distinctio veniam magni.</p>
+        <NavLink href= {in_route}>
+          <button>Sign in</button>
+        </NavLink>
+        <NavLink href={up_route}>
+          <button>Sign up</button>
+        </NavLink>
         </>
       )
     }
