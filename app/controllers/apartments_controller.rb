47faf2b492666d127a64e1f0c201d989474a3016
BCarta7 <cannolidude@msn.com>
2023-01-09 16:41:21 -0800

    only testing left

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -3,4 +3,13 @@ class ApartmentsController < ApplicationController
         @apartments = Apartment.all
         render json: @apartments
     end
+    def create
+        apartment = Apartment.create(apartment_params)
+        render json: apartment
+    end
+
+    private
+    def apartment_params
+        params.require(:apartment).permit(:street, :city, :state, :manager, :email, :price, :bedrooms, :bathrooms, :pets, :image, :user_id)
+    end
 end
--- spec/requests/apartments_spec.rb
+++ spec/requests/apartments_spec.rb
@@ -27,4 +27,31 @@ RSpec.describe "Apartments", type: :request do
       expect(apartment.length).to eq 1
     end
   end
+  describe "POST /create" do
+    it "creates an apartment" do
+        user = User.create(
+        email: "test@example.com", 
+        password: "password", 
+        password_confirmation: "password")
+        apartment_params = {user.apartments: {
+          street: "4 Privet Drive",
+          city: "Little Whinging",
+          state: "Surrey",
+          manager: "Mr. Potter",
+          email: "potter@example.com",
+          price: 2000,
+          bedrooms: 3,
+          bathrooms: 2,
+          pets: "yes",
+          image: "https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg"
+        }
+      }
+
+      post '/apartments', params: apartment_params
+      expect(response).to have_http_status(200)
+
+      apartment = Apartment.first
+      expect(apartment.state).to eq 'Surrey'
+    end
+  end
 end
